51e391bdf474bdf8b21987564843ee665e60a795
Jacque Carvalho <ds.jacque@gmail.com>
2016-04-14 11:31:32 -0300

    Update Getting Started.R

--- W1/Getting Started.R
+++ W1/Getting Started.R
@@ -18,3 +18,31 @@ sum
 
 #________________________________________________________________________________________________________________________________________
 
+#1
+femaleMW <- read.csv("femaleMiceWeights.csv")
+summary(femaleMW)
+head(femaleMW)
+names(femaleMW)
+
+#2. extract specific rows and specific columns
+femaleMW[12,2]
+
+#3.  $ character to extract a column from a table and return it as a vector
+weights <-femaleMW$Bodyweight
+weights[11]
+
+#4. "length" function returns the number of elements in a vector
+length(femaleMW$Diet)
+
+#5. To create a vector with the numbers 3 to 7, we can use seq(3,7) or, because they are consecutive, 3:7
+#   View the data and determine what rows are associated with the high fat or hf diet.
+#   Then use the mean function to compute the average weight of thes
+View(femaleMW)
+mean(weights[13:24])
+
+#6. 
+?sample
+set.seed(1)
+i <- sample(13:24, 1)
+femaleMW$Bodyweight[i]
+
